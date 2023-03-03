28da82b72d2cb915e2042d47c1b24ef8a6797f9a
Syl8n <magi8520@gmail.com>
2023-03-04 01:55:54 +0900

    Initial commit

---
+++ src/main/java/com/example/citest/domain/hello/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.citest.domain.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "Welcome to COMP367";
+    }
+}
